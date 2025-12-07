26a5321c1c6b216a14a390976b370de20b85a4f8
Elior <elior@gmail.com>
2025-12-07 21:26:59 +0200

    with swagger

--- src/main/java/com/example/Project1/controller/HelloController.java
+++ src/main/java/com/example/Project1/controller/HelloController.java
@@ -1,13 +1,16 @@
 package com.example.Project1.controller;
 
+import com.example.Project1.objects.Student;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import static java.lang.Character.getName;
 
-    @RestController
+
+@RestController
     @RequestMapping("/api")
     public class HelloController {
 
@@ -20,4 +23,10 @@ import org.springframework.web.bind.annotation.RestController;
         public ResponseEntity<?> bye(){
             return new ResponseEntity<>("bye World!", HttpStatus.OK);
         }
+        Student student = new Student();
+        @RequestMapping(value = "/Student/name", method = RequestMethod.GET)
+        public ResponseEntity<?> name(){
+            return new ResponseEntity<>("name=" + student.getName(), HttpStatus.OK);
+        }
+
 }
---
+++ src/main/java/com/example/Project1/objects/Student.java
@@ -0,0 +1,12 @@
+package com.example.Project1.objects;
+
+import lombok.*;
+
+@Getter
+@Setter
+
+public class Student {
+   private String name = "david";
+    private int age;
+    private double avg;
+}
